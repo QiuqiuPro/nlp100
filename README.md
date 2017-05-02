8be1c34703e063ac6b03c2f8cb84ad46ceb1cf3b
QiuLiu <qiu.gits@gmail.com>
2017-05-02 23:05:52 +0900

    chapter 1 fin.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # 言語処理100本ノック
 Neural Language Processing
 http://www.cl.ecei.tohoku.ac.jp/nlp100/
+
+#### 参照先
+解答例　http://qiita.com/gamma1129/items/37bf660cf4e4b21d4267  
+かじゅ　https://github.com/kajyuuen/nlp100knock
--- chapter1/00-09.py
+++ chapter1/00-09.py
@@ -1,21 +1,21 @@
 
 # coding: utf-8
 
-# In[20]:
+# In[1]:
 
 # 00.
 s = 'stressed'
 s[-1::-1]
 
 
-# In[21]:
+# In[2]:
 
 # 01.
 s = 'パタトクカシーー'
 s[1::2]
 
 
-# In[36]:
+# In[3]:
 
 # 02.
 p = 'パトカー'
@@ -26,7 +26,7 @@ for i in range(4):
 s
 
 
-# In[39]:
+# In[4]:
 
 # 03.
 s = "Now I need a drink, alcoholic of course,     after the heavy lectures involving quantum mechanics."
@@ -34,59 +34,21 @@ sl = list(map(len, s.split()))
 sl
 
 
-# In[48]:
+# In[5]:
 
 # 04.
 s = "Hi He Lied Because Boron Could Not Oxidize Fluorine.    New Nations Might Also Sign Peace Security Clause. Arthur King Can."
 sd = {}
-for i, w in enumerate(map(lambda a: a[:2], s.split())):
-    if i in [1, 5, 6, 7, 8, 9, 15, 16, 19]:
-        sd[w[:1]] = i
-    sd[w] = i
+for i, w in enumerate(list(s.split())):
+    j = i+1
+    if j in [1, 5, 6, 7, 8, 9, 15, 16, 19]:
+        sd[w[:1]] = j
+    else:
+        sd[w[:2]] = j
 sd
 
 
-# In[73]:
-
-# 05.
-# http://d.hatena.ne.jp/jetbead/20110904/1315147133
-def biGram(sorl):
-    if type(sorl) == str:
-        sl = sorl.split()
-        if type(sl) != list:
-            sl = [sl]
-    else:
-        sl = sorl
-    print(sl)
-    word_biGram = set()
-    sent_biGram = set()
-    i = 0
-    while i < len(sl):
-        if i != len(sl)-1: #not last one. cuz until n-1;
-            word_biGram.add(sl[i]+'-'+sl[i+1])
-        j = 0
-        while j < len(sl[i])-1:
-            sent_biGram.add(sl[i][j]+sl[i][j+1])
-            j += 1
-        i += 1
-    return {'w': word_biGram, 's': sent_biGram}
-
-biGram("I am an NLPer")
-
-
-# In[74]:
-
-# 06.
-s1 = "paraparaparadise"
-s2 = "paragraph"
-biGram(s1)
-X = biGram(s1)['w']
-Y = biGram(s2)['w']
-# print(X, Y)
-# print(X or Y)
-
-
-# In[44]:
+# In[6]:
 
 # 与えられたシーケンス（文字列やリストなど）からn-gramを作る関数を作成せよ．
 # この関数を用い，"I am an NLPer"という文から単語bi-gram，文字bi-gramを得よ．
@@ -108,7 +70,7 @@ for w in s.split():
 ngram(s); print()
 
 
-# In[57]:
+# In[7]:
 
 def ngram(sequence, n = 2, sep = ''):
     #リストなら単語区切り、文字列なら単語が来ると想定
@@ -129,7 +91,7 @@ print(X&Y)
 print(X-Y, Y-X, X^Y)
 
 
-# In[58]:
+# In[8]:
 
 # 引数x, y, zを受け取り「x時のyはz」という文字列を返す関数を実装せよ．さらに，x=12, y="気温", z=22.4として，実行結果を確認せよ．
 def ps(x,y,z):
@@ -138,15 +100,25 @@ def ps(x,y,z):
 ps(12,'気温',22.4)
 
 
-# In[59]:
+# In[9]:
 
 # 関数cipherを実装せよ．
 # 英小文字ならば(219 - 文字コード)の文字に置換
 # その他の文字はそのまま出力
 # この関数を用い，英語のメッセージを暗号化・復号化せよ．
+def cipher(w):
+    r = ''
+    for c in w[:]:
+        if c.islower():
+            c = chr( 219 - ord(c) )
+        r += c
+    return r
+
+cipher('Hello world!')
+cipher(cipher('Hello world again!'))
 
 
-# In[73]:
+# In[10]:
 
 # スペースで区切られた単語列に対して，各単語の先頭と末尾の文字は残し，それ以外の文字の順序をランダムに並び替えるプログラムを作成せよ．
 # ただし，長さが４以下の単語は並び替えないこととする．適当な英語の文
@@ -155,22 +127,16 @@ s = "I couldn't believe that I could actually understand what I was reading : th
 # を与え，その実行結果を確認せよ．
 def shuffleWord(sentence):
     sts = sentence.split()
-    rslt = []
+    result = []
     for i in sts:
         if len(i) > 4:
             mid = list(i[1:-1])
             shuffle(mid)
-            rslt.append() i[0] + ''.join(mid) + i[-1]
-    return ' '.join(sts)
+            i = i[0] + ''.join(mid) + i[-1]
+        result.append(i)
+    return ' '.join(result)
 
 shuffleWord(s)
-
-
-# In[72]:
-
-a = [1,2,3]
-shuffle(a)
-print(a)
 
 
 # In[ ]:
